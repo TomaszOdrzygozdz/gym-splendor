e45ebd5f9b172314153388bfc7956594ff47d48c
Tomasz Odrzygóźdź <tomeko314@gmail.com>
2019-10-13 11:46:24 +0200

    Basic classes im envs.mechanics: pre_card, noble, card

---
+++ envs/mechanics/card.py
@@ -0,0 +1,27 @@
+from envs.mechanics.enums import Row, GemColor
+from typing import Dict
+from envs.mechanics.pre_card import PreCard
+
+
+class Card(PreCard):
+    def __init__(self,
+                 name: str,
+                 id: int,
+                 row: Row,
+                 price: Dict[GemColor, int],
+                 discount_profit: GemColor,
+                 victory_points: int) -> None:
+        """Parameters:
+        _ _ _ _ _ _
+        name: Name of the card (string).
+        id: Identificator of the card (integer). Useful for one-hot encoding of the card.
+        row: Row to which this card belong (Cheap, Medium or Expensive).
+        price: Dictionary with keys being gem color and values being integers. This dictionary describes the price of
+        card.
+        profit: Discount that this card gives when buying next cards.
+        vistory_points: Victory points given by this card."""
+        super().__init__(name, id, price, victory_points)
+        self.row = row
+        self.discount_profit = discount_profit
+
+
---
+++ envs/mechanics/enums.py
@@ -0,0 +1,38 @@
+'''This file contains enums that are used in the game.'''
+
+from enum import Enum, auto
+
+class Row(Enum):
+    ''' Each card bolngs to one of 3 possible rows. Rows stand for prestige of card '''
+    CHEAP = 0
+    MEDIUM = 1
+    EXPENSIVE = 2
+
+class PlayerId(Enum):
+    '''In games are two players with id's: PLAYER_A and PLAYER_B'''
+    PLAYER_A = auto()
+    PLAYER_B = auto()
+
+
+class GemColor(Enum):
+    '''
+    This class represents colors of gems that are used in the game.
+    '''
+    # We do not use auto() to have control of how gem colors are encoded to vectors (to make this encoding consistent).
+    GOLD = 0
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+    WHITE = 4
+    BLACK = 5
+
+
+class MoveType(Enum):
+    '''
+    This class represents four main types of moves.
+    '''
+    # We do not use auto() to have control of how move types are encoded to vectors (to make this encoding consistent).
+    BUY_CARD = 0
+    BUY_RESERVED_CARD = 1
+    MODIFY_COINS = 2
+    RESERVE_CARD = 3
