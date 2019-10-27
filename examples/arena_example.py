fe9a0bca2aa2a2ac41561dddb5bde132dc364ebe
Tomasz Odrzygóźdź <tomeko314@gmail.com>
2019-10-27 16:22:30 +0100

    Arena example 2.

--- examples/arena_example.py
+++ examples/arena_example.py
@@ -1,9 +1,12 @@
 from agents.random_agent import RandomAgent
 from arena import Arena
+import time
 
 fight_pit = Arena()
 
 goku = RandomAgent(distribution='uniform_on_types')
 gohan = RandomAgent(distribution='uniform')
 
+time_dupa = time.time()
 fight_pit.run_one_game([goku, gohan], starting_player_id=0)
+print(time.time() - time_dupa)
