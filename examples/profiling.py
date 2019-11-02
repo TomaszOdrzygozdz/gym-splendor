7921c7a9b1c763958e04b04d1eeceee8d0be190d
Gleader <40356698+Gleader@users.noreply.github.com>
2019-11-02 23:55:42 +0100

    Fix of MaxMin and GreedySearch agent added

--- examples/profiling.py
+++ examples/profiling.py
@@ -1,6 +1,7 @@
 from agents.random_agent import RandomAgent
 from agents.greedy_agent import GreedyAgent
 from agents.minmax_agent import MinMaxAgent
+from agents.greedysearch_agent import GreedySearchAgent
 from arena.arena import Arena
 
 fight_pit = Arena()
@@ -11,7 +12,7 @@ goku2 = RandomAgent(distribution='uniform')
 #gohan = RandomAgent(distribution='uniform')
 #goku = GreedyAgent(weight = 0.3)
 gohan = GreedyAgent(weight = 0.1)
-
+goku = GreedySearchAgent(name = "GS", depth = 5)
 goku = MinMaxAgent(name = "MinMax", depth = 3)
 
 
@@ -25,7 +26,7 @@ goku = MinMaxAgent(name = "MinMax", depth = 3)
 # fight_pit.run_one_duel([goku, gohan], starting_agent_id=0, render_game=True)
 
 
-print(fight_pit.run_many_games([goku, gohan], number_of_games = 100, shuffle_agents=False))
+print(fight_pit.run_many_duels([goku, gohan], number_of_games = 10, shuffle_agents=False))
 
 # time_dupa = time.time()
 # for i in range(100):
