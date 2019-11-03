cc922d1668ca41fa167b4e12ee76f6665507b6dc
Tomasz Odrzygóźdź <tomeko314@gmail.com>
2019-11-03 21:59:44 +0100

    Separated Greedy and GreedyBoost

--- agents/greedy_agent_boost.py
+++ agents/greedy_agent_boost.py
@@ -5,11 +5,11 @@ from gym_splendor_code.envs.mechanics.action import Action
 from gym_splendor_code.envs.mechanics.game_settings import POINTS_TO_WIN
 
 
-class GreedyAgent(Agent):
+class GreedyAgentBoost(Agent):
 
     def __init__(self,
                 name: str = "Greedy",
-                weight: float = 0.1):
+                weight: list = []):
         """:param:
         distribution: argument determining how action is chosen at random. Possible options are:
         uniform - this draws from random distribution on all legal action
@@ -22,8 +22,9 @@ class GreedyAgent(Agent):
 
         #we create own gym-splendor enivronemt to have access to its functionality
         #We specify the name of the agent
-        self.name = name + ' ' + str(weight)
+        self.name = name + ' ' + str(weight) + ' ' + str(Agent.agents_created)
         self.weight = weight
+        self.normalize_weight()
 
     def choose_action(self, observation) -> Action:
 
@@ -32,12 +33,14 @@ class GreedyAgent(Agent):
         self.env.update_actions()
         current_points = self.env.current_state_of_the_game.active_players_hand().number_of_my_points()
 
-        if len(self.env.action_space.list_of_actions)>0:
+        if len(self.env.action_space.list_of_actions):
             actions = []
             potential_reward_max = -20
             for action in self.env.action_space.list_of_actions:
                 ae = action.evaluate(self.env.current_state_of_the_game)
-                potential_reward = (np.floor((current_points + ae["card"][2])/POINTS_TO_WIN)*100 + 10 * ae["card"][2] + 2 *ae["nobles"] + ae["card"][0] + self.weight * sum(ae["gems_flow"]))
+                potential_reward = (np.floor((current_points + ae["card"][2])/POINTS_TO_WIN) * self.weight[0] +\
+                                    self.weight[1] * ae["card"][2] + self.weight[2] *ae["nobles"] +\
+                                     self.weight[3] * ae["card"][0] + self.weight[4] * sum(ae["gems_flow"]))
                 if potential_reward > potential_reward_max:
                     potential_reward_max = potential_reward
                     actions = []
@@ -49,3 +52,13 @@ class GreedyAgent(Agent):
 
         else:
             return None
+
+    def normalize_weight(self):
+        if np.linalg.norm(self.weight) > 0:
+            self.weight = self.weight/np.linalg.norm(self.weight)
+
+    def update_weight(self, list, lr, ratio):
+        list = list/np.linalg.norm(list)
+        lr = lr * ratio
+        self.weight = [a + b *lr for a, b in zip(self.weight, list)]
+        self.normalize_weight()
