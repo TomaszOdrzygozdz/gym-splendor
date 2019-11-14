b670291a7d4e5f5e011a5be208251ff2c045b646
Gleader <40356698+Gleader@users.noreply.github.com>
2019-11-15 00:08:31 +0100

    2 versions of splendor env (basic and wrapped)

--- gym_splendor_code/__init__.py
+++ gym_splendor_code/__init__.py
@@ -5,6 +5,14 @@ logger = logging.getLogger(__name__)
 
 register(
     id='splendor-v0',
+    entry_point='gym_splendor_code.envs.splendor:SplendorEnv',
+    #timestep_limit=1000,
+    #reward_threshold=1.0,
+    #nondeterministic = True,
+)
+
+register(
+    id='splendor-v1',
     entry_point='gym_splendor_code.envs.splendor_wrapper:SplendorWrapperEnv',
     #timestep_limit=1000,
     #reward_threshold=1.0,
