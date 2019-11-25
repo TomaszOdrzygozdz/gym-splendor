c7eaa0512f2ca51e03f8b71cd5880502782ece2b
Gleader <40356698+Gleader@users.noreply.github.com>
2019-11-25 20:48:15 +0100

    Fixes on moves generation

--- gym_splendor_code/envs/utils/utils_functions.py
+++ gym_splendor_code/envs/utils/utils_functions.py
@@ -23,3 +23,17 @@ def tuple_of_gems_to_gems_collection(tuple_of_gems: Tuple[GemColor], val = 1, re
     for i, element in enumerate(return_colors):
         gems_dict[element] -= return_val[i]
     return GemsCollection(gems_dict)
+
+
+def colors_to_gems_collection(tuple_of_gems) -> GemsCollection:
+    """Return a gems collection constructed from list of colors:
+    Parameters:
+     _ _ _ _ _ _
+        tuple_of_gems: Tuple of gems (with possible repetitions).
+
+    Returns: A gems collections. Example:
+    (red, red, blue, green) is transformed to GemsCollection({red:2, blue:1, green:1, white:0, black:0, gold:0})."""
+    gems_dict = {gem_color: 0 for gem_color in GemColor}
+    for element in tuple_of_gems:
+        gems_dict[element] += 1
+    return GemsCollection(gems_dict)
