bb00368ef6dce4d2c76ad4e61a415d3c57565a2a
Tomasz Odrzygóźdź <tomeko314@gmail.com>
2019-12-05 15:51:26 +0100

    Collecting data for cloning mpi

--- run_experiment4.py
+++ run_experiment4.py
@@ -13,7 +13,7 @@ agent1 = RandomAgent(distribution='first_buy')
 agent1a = RandomAgent(distribution='first_buy')
 agent2 = MinMaxAgent(collect_stats=True)
 agent3 = DenseNNAgent('agents\weights\minmax_20_games_depth_3.h5')
-agent3a = DenseNNAgent()
+#agent3a = DenseNNAgent()
 agent4 = GreedyAgentBoost()
 
 arek = MultiArena()
@@ -22,12 +22,14 @@ arek = MultiArena()
 
 print('making data with random first buy')
 
-arek.run_many_duels('deterministic', [agent1, agent2], n_games=200, n_proc_per_agent=1)
-agent2.dump_action_scores('200_games_against_random_first_buy')
+
+
+arek.run_many_duels('deterministic', [agent1, agent2], n_games=2, n_proc_per_agent=24)
+agent2.dump_action_scores('data/200_games_against_random_first_buy_my_rank_{}'.format(my_rank))
 
 print('making data with greedy boost')
-arek.run_many_duels('deterministic', [agent4, agent2], n_games=200, n_proc_per_agent=1)
-agent2.dump_action_scores('200_games_against_greedy_boost')
+arek.run_many_duels('deterministic', [agent4, agent2], n_games=2, n_proc_per_agent=24)
+agent2.dump_action_scores('data/200_games_against_greedy_boost_my_rank_{}'.format(my_rank))
 
 
 #testing
