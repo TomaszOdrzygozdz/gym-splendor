52fc2f02a0c555f72963980d33a71cd9ab775c08
Tomasz Odrzygóźdź <tomeko314@gmail.com>
2019-12-13 21:05:42 +0100

    Experiment 7 better.

--- run_experiment7.py
+++ run_experiment7.py
@@ -6,6 +6,6 @@ import cProfile
 
 staty = cProfile.Profile()
 
-fufu = SelfPlayTrainer('dqn', iteration_limit=15, rollout_repetition=5, choose_best=0.2)
+fufu = SelfPlayTrainer('dqn', iteration_limit=24, rollout_repetition=2, choose_best=0.2)
 staty.run('fufu.full_training(10, 0.1, 2)')
 staty.dump_stats('self_play.prof')
