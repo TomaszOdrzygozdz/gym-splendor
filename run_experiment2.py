6523550c272e0306261600af182d0e813d13c34c
TomaszOdrzygozdz <tomeko314@gmail.com>
2020-02-07 09:40:53 +0100

    Produce data corrections.

--- run_experiment2.py
+++ run_experiment2.py
@@ -1,6 +1,6 @@
 
 from training_data.data_generation.gen_data_lvl0 import produce_data, load_data_for_model
-produce_data(10, 0.25, 1, 'all_vs_all.pickle' , '/home/tomasz/ML_Research/splendor/gym-splendor/supervised_data/lvl1')
+produce_data(10, 0.25, 1, 'all_vs_all.pickle' , 'supervised_data/lvl1')
 
 # data = load_data_for_model('/home/tomasz/ML_Research/splendor/gym-splendor/supervised_data/combined.pickle')
 #
