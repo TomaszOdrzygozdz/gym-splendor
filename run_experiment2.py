b1cbb26559f98c1228e5e6eb90b1eda1ddba0048
TomaszOdrzygozdz <tomeko314@gmail.com>
2020-02-10 13:01:05 +0100

    Producing large dataset for training 2.

--- run_experiment2.py
+++ run_experiment2.py
@@ -4,8 +4,8 @@ from training_data.data_generation.gen_data_lvl0 import produce_data, load_data_
 
 #produce_data(10, 0.25, 5, 'all_vs_all.pickle', '/home/tomasz/ML_Research/splendor/gym-splendor/supervised_data/test_lvl0')
 
-pick_data_for_training(range(0,2), '/net/people/plgtodrzygozdz/gym-splendor/supervised_data/lvl3',
-                       '/net/people/plgtodrzygozdz/gym-splendor/supervised_data/lvl_3_training')
+pick_data_for_training(range(0,2), '/net/archive/groups/plggluna/plgtodrzygozdz/lvl3/lvl3',
+                       '/net/archive/groups/plggluna/plgtodrzygozdz/lvl3_training')
 
 #data = load_data_for_model('/home/tomasz/ML_Research/splendor/gym-splendor/cluster_stuff/temp/proc_205_all_vs_all.pickle')
 
