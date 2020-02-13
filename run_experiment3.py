cb2d656518eb47e68aff7da9b11b2d800ed76075
TomaszOdrzygozdz <tomeko314@gmail.com>
2020-02-13 20:32:29 +0100

    fixed import errors

--- run_experiment3.py
+++ run_experiment3.py
@@ -1,6 +1,6 @@
 import gin
 from nn_models.architectures.average_pool_v0 import StateEvaluator
-gin.parse_config_file('/home/tomasz/ML_Research/splendor/gym-splendor/nn_models/experiments/series_1/experiment_1/params_v1.gin')
+gin.parse_config_file('nn_models/experiments/series_1/experiment_1/run_experiment.py')
 
 model = StateEvaluator()
 model.train_network_on_many_sets('/net/archive/groups/plggluna/plgtodrzygozdz/lvl1/train_epochs/epoch_',
