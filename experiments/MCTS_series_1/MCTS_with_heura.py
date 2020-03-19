3a6e90b5021d2611407d493c119472cbb372b168
TomaszOdrzygozdz <tomeko314@gmail.com>
2020-03-19 01:19:41 +0100

    Cluster run

--- experiments/MCTS_series_1/MCTS_with_heura.py
+++ experiments/MCTS_series_1/MCTS_with_heura.py
@@ -7,13 +7,12 @@ from nn_models.architectures.average_pool_v0 import StateEncoder, ValueRegressor
 gin.parse_config_file('experiments/MCTS_series_1/params.gin')
 from monte_carlo_tree_search.trainers.MCTS_value_training import MCTS_value_trainer
 
-
 def run_experiment():
     #trainer = MCTS_value_trainer(weights='/net/archive/groups/plggluna/plgtodrzygozdz/lvl1/weights/w1.h5')
     trainer = MCTS_value_trainer()
 
     if not CLUSTER:
-        trainer.run_training_games_multi_process(GreedyAgentBoost(), epochs=1, mcts_passes=150, n_test_games=10, exploration_ceofficient=0.61,
+        trainer.run_training_games_multi_process(GreedyAgentBoost(), epochs=1, mcts_passes=50, n_test_games=10, exploration_ceofficient=0.61,
                                    experiment_name='MCTS with NN', value_threshold=0.8,
                                                  weights_path='/home/tomasz/ML_Research/splendor/gym-splendor/archive/weights_tt1/',
                                                  confidence_threshold=0.1, count_threshold=50)
@@ -22,4 +21,4 @@ def run_experiment():
         trainer.run_training_games_multi_process(GreedyAgentBoost(), epochs=200, mcts_passes=150, n_test_games=96,  exploration_ceofficient=0.61,
                                    experiment_name='MCTS with NN', value_threshold=0.8,
                                                  weights_path='/net/archive/groups/plggluna/plgtodrzygozdz/weights_temp/',
-                                                 confidence_threshold=0.1, count_threshold=6)
+                                                 confidence_threshold=0.1, count_threshold=50)
--- experiments/data_production/data_production_1M.py
+++ experiments/data_production/data_production_1M.py
@@ -1,7 +1,7 @@
 from training_data.data_generation.gen_data_lvl0 import produce_data, pick_data_for_training, evaluate_states
 
 
-def produce_large_data():
+def run_experiment():
     #produce_data(0,0.1, 150000, 'large', '/net/archive/groups/plggluna/plgtodrzygozdz/large_states_set/raw_states')
     # pick_data_for_training(range(15), '/net/archive/groups/plggluna/plgtodrzygozdz/large_states_set/raw_states',
     #                        '/net/archive/groups/plggluna/plgtodrzygozdz/large_states_set/picked_states', 5)
